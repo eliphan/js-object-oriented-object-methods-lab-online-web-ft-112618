e557bbb755ffd2eced243730dcc7ce3ed3f95b2f
Elise Phan <aliceconnic@gmail.com>
2019-03-15 00:17:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ function BoardMember(name, homeState, training) {
   this.homeState = homeState;
   this.training = training;
 
+  this.veto = "No, I must disagree"
 
-this.sayHi = function(){
-  return (`Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`);
-  }
+
+  this.sayHi = function(){
+    return (`Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`);
+    }
   
 }
