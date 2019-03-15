d0414ca85a41b60eb0e5515efb707264c0207d02
Elise Phan <aliceconnic@gmail.com>
2019-03-15 00:15:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ function BoardMember(name, homeState, training) {
   this.name = name; 
   this.homeState = homeState;
   this.training = training;
-  this.sayHi = function(){
-    console.log(`Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`);
-  }
+
 }
 
