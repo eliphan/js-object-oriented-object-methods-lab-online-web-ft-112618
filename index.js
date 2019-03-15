9e16971f5b4174a6cde8026285fb9083bc44defb
Elise Phan <aliceconnic@gmail.com>
2019-03-15 00:10:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ function BoardMember(name, homeState, training) {
   this.name = name; 
   this.homeState = homeState;
   this.training = training;
-  
+  this.sayHi = function(){
+    console.log("Hello everybody!");
+  }
 }
 
